dc6eea23b633533de711f1d4ca11b5c1f1f14c78
atishay-nar <atishayna@gmail.com>
2025-07-08 10:27:08 -0700

    updated model to use same data as quantum model

--- src/sample.py
+++ src/sample.py
@@ -11,6 +11,7 @@ from torchvision import transforms
 from PIL import Image
 from image_gpt import ImageGPT
 from utils import quantize, unquantize
+from reduced_mnist import ReducedMNISTDataset
 
 # set device
 DEVICE = (
@@ -28,14 +29,14 @@ if torch.cuda.is_available():
 def generate(model, context, length, num_samples=1):
     output = context.unsqueeze(0).repeat(num_samples, 1)  # add batch size of num_samples so shape [seq len, batch]
 
-    # precfg_dict
+    # predict
     with torch.no_grad():
         for _ in tqdm(range(length), leave=False, desc=f"Generating"):
             logits = model(output)               # (batch, seq, vocab)
             logits = logits[:, -1, :]            # (batch, vocab)
             probs = F.softmax(logits, dim=-1)  # convert logits to probabilities
             pred = torch.multinomial(probs, num_samples=1) # sample from the distribution
-            output = torch.cat((output, pred), dim=1)  # append the precfg_dicted token to the output
+            output = torch.cat((output, pred), dim=1)  # append the predicted token to the output
 
     return output
 
@@ -54,14 +55,14 @@ def sample(cfg):
     centroids = torch.tensor(np.load(centroids_path)).to(DEVICE)
 
     # load dataset
-    test_data = torchvision.datasets.MNIST(root=cfg.DATA_DIR, train=False, download=True, transform=transforms.ToTensor())
+    test_data = ReducedMNISTDataset(cfg, train=False)  # load test dataset
     loader = iter(DataLoader(test_data, batch_size=1, shuffle=True))
 
     # rows for each image in final figure
     rows = []
     for example in tqdm(range(cfg.num_examples), desc="Sampling Images"):
         # get random image
-        img, _ = next(loader)
+        img = next(loader)
         img = img[0].to(DEVICE)
 
 
@@ -95,8 +96,8 @@ def sample(cfg):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--ckpt_num", required=True, type=int)
-    parser.add_argument("--n_examples", default=5, type=int)
-    parser.add_argument("--n_samples", default=5, type=int)
+    parser.add_argument("--n_examples", default=3, type=int)
+    parser.add_argument("--n_samples", default=3, type=int)
     args, _ = parser.parse_known_args()
     cfg_dict = yaml.safe_load(open("configs.yml", "r"))
     cfg_dict["checkpoint"] = args.ckpt_num
