caddbc08756f904fdca2f4a50fccc207bbfd727a
atishay-nar <atishayna@gmail.com>
2025-07-10 11:44:05 -0700

    updated sample.py to allow for sepcific checkpoints, avoid producing images from duplicate classes before visiting all classes, and evaluate SSIM and PSNR

--- src/sample.py
+++ src/sample.py
@@ -12,6 +12,8 @@ from PIL import Image
 from image_gpt import ImageGPT
 from utils import quantize, unquantize
 from reduced_mnist import ReducedMNISTDataset
+from utils import quantize, unquantize
+from skimage.metrics import structural_similarity, peak_signal_noise_ratio
 
 # set device
 DEVICE = (
@@ -44,7 +46,10 @@ def sample(cfg):
     os.makedirs('./figures', exist_ok=True)
   
     # create model and load checkpoint
-    ckpt = torch.load(f'./checkpoints/image_gpt_epoch_{cfg.checkpoint}.pth', map_location=DEVICE)
+    if cfg.specific:
+        ckpt = torch.load(cfg.checkpoint, map_location=DEVICE)
+    else:
+        ckpt = torch.load(f'./checkpoints/image_gpt_epoch_{cfg.checkpoint}.pth', map_location=DEVICE)
     model = ImageGPT(cfg).to(DEVICE)
     model.load_state_dict(ckpt)
     model.eval()
@@ -60,11 +65,20 @@ def sample(cfg):
 
     # rows for each image in final figure
     rows = []
+    visited = set()
+    ssim = []
+    psnr = []
     for example in tqdm(range(cfg.num_examples), desc="Sampling Images"):
         # get random image
         img, label = next(loader)
+        
+        # ensure no duplicate classes before all classes are visited
+        if len(visited) < len(cfg.CLASSES):
+            while label in visited:
+                img, label = next(loader)
+            visited.add(label.item())
+        
         img = img[0].to(DEVICE)
-
 
         # quantize image to tokens
         img = quantize(img, centroids).cpu().numpy() # get tokens and flatten into seq
@@ -81,6 +95,18 @@ def sample(cfg):
         pred = generate(model, context, int(len(tokens) / 2), num_samples=cfg.num_samples).cpu().numpy()
         pred = pred.reshape(-1, cfg.IMAGE_SIZE, cfg.IMAGE_SIZE)  # reshape to image size
 
+        # evluate the generated images
+        for pred_img in pred:
+            # unquantize the truth and predicted
+            gen = unquantize(pred_img, centroids).squeeze(-1).cpu().numpy() # squeeze because we do not need channels
+            truth = unquantize(img, centroids).squeeze(-1).cpu().numpy()
+
+            # range 
+            data_range = max(gen.max(), truth.max()) - min(gen.min(), truth.min())
+            # calculate SSIM and PSNR
+            ssim.append(structural_similarity(truth, gen, data_range=data_range))
+            psnr.append(peak_signal_noise_ratio(truth, gen, data_range=data_range))
+
         # add example to rows
         rows.append(np.concatenate([context_img[None, ...], pred, img[None, ...]], axis=0)) # 
     
@@ -90,17 +116,28 @@ def sample(cfg):
     fig = unquantize(fig.swapaxes(1, 2).reshape(h * nrow, w * ncol), centroids).cpu().numpy()
     fig = (fig * 255).round().astype(np.uint8)
     pic = Image.fromarray(np.squeeze(fig))
-    pic.save(f"./figures/sample_at_epoch_{cfg.checkpoint}.png")
+
+    if cfg.specific:
+        pic.save(f"./figures/sample_from_{cfg.checkpoint.split('/')[-1].split('.')[0]}.png")
+    else:
+        pic.save(f"./figures/sample_at_epoch_{cfg.checkpoint}.png")
     
+    print(f'''
+    SSIM: mean {np.mean(ssim):.4f} and std {np.std(ssim):.4f}
+    PSNR: mean {np.mean(psnr):.4f} and std {np.std(psnr):.4f}
+''')
       
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--ckpt_num", required=True, type=int)
+    parser.add_argument("--ckpt", required=True, type=str)
+    parser.add_argument("--specific", default=False, type=bool)
     parser.add_argument("--n_examples", default=3, type=int)
     parser.add_argument("--n_samples", default=3, type=int)
     args, _ = parser.parse_known_args()
+
     cfg_dict = yaml.safe_load(open("configs.yml", "r"))
-    cfg_dict["checkpoint"] = args.ckpt_num
+    cfg_dict["specific"] = args.specific
+    cfg_dict["checkpoint"] = args.ckpt
     cfg_dict["num_examples"] = args.n_examples
     cfg_dict["num_samples"] = args.n_samples
     cfg = argparse.Namespace(**cfg_dict)
